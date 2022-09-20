7132c4136ff0afab3762f2a8635cf9bd66331d54
AlexandraMihalevschi <68810700+AlexandraMihalevschi@users.noreply.github.com>
2022-09-20 23:49:14 +0300

    Update Tpc_20_09_2022.py

--- Tpc_20_09_2022.py
+++ Tpc_20_09_2022.py
@@ -6,19 +6,19 @@ b = int(input('Al doilea numar:'))
 def suma(x, y):
     s = a + b
     return s
-print("A. Suma numerelor", suma(a, b))
+print("A. Suma numerelor.", suma(a, b))
 
 #B. Produsul numerelor
 def produs(x, y):
     p = a * b
     return p
-print("B. Produsul numerelor", produs(a, b))
+print("B. Produsul numerelor.", produs(a, b))
 
 #C. Media aritmetica
 def med_aritmetica(x, y):
     m = (x+y)/2
     return m
-print("C. Media aritmetica", med_aritmetica(a, b))
+print("C. Media aritmetica.", med_aritmetica(a, b))
 
 #D. Cel mai mare divizor comun
 def cmmdc(x, y):
@@ -28,26 +28,26 @@ def cmmdc(x, y):
             div_com = i
         i = i+1
     return div_com
-print("D. Cel mai mare divizor comun", cmmdc(a, b))
+print("D. Cel mai mare divizor comun.", cmmdc(a, b))
 
 #E. Cel mai mic multiplu comun
 def cmmmc(x, y):
     return int((x/cmmdc(x, y))*y)
-print("E. Cel mai mic multiplu comun", cmmmc(a, b))
+print("E. Cel mai mic multiplu comun.", cmmmc(a, b))
 
 #F. Numarul minim
 def minim(x, y):
     if x<y:
         return x
     else: return y
-print("F. Numarul minim", minim(a, b))
+print("F. Numarul minim.", minim(a, b))
 
 #G. Numarul maxim
 def maxim(x, y):
     if x>y:
         return x
     else: return y
-print("F. Numarul maxim", maxim(a, b))
+print("F. Numarul maxim.", maxim(a, b))
 
 #H. Suma numerelor citite in timpul programului
 def suma_locala():
@@ -55,7 +55,7 @@ def suma_locala():
     b = int(input("Dati al doilea nr:"))
     c = a + b
     return c
-print("H. Suma numerelor citite in timpul programului", suma_locala())
+print("H. Suma numerelor citite in timpul programului.", suma_locala())
 
 #I. Produsul numerelor citite in timpul programului
 def produsul_local():
@@ -63,7 +63,7 @@ def produsul_local():
     l = int(input("Dati al doilea termen:"))
     c = k * l
     return c
-print("I. Produsul numerelor citite in timpul programului", produsul_local())
+print("I. Produsul numerelor citite in timpul programului.", produsul_local())
 
 #J. Toti divizorii
 def toti_divizorii(x, y):
@@ -75,23 +75,23 @@ def toti_divizorii(x, y):
     for i in range(1, y+1):
         if y%i==0:
             divizori2.append(i)
-    return divizori1, divizori2
-print("J. Toti divizorii", toti_divizorii(a, b))
+    divizor = list(set(divizori1+divizori2))
+    return divizor
+print("J. Toti divizorii.", toti_divizorii(a, b))
 
 #K. Cinci multipli comuni
 def multipli_comuni(x, y):
     mul_com1 = []
-    mul_com2 = []
-    for i in range(1,21):
+    mul_com2 = []       
+    for i in range(1,x*y):
         m = i*x
         mul_com1.append(int(m))
-    for i in range(1,21):
+    for i in range(1,x*y):
         n = i*y
         mul_com2.append(int(n))
-    mul_com = list(set(mul_com1+mul_com2))
-    mul_com = mul_com[0:5]
-    return mul_com
-print("K. Cinci multipli comuni", multipli_comuni(a, b))
+    mul_com = list(set(mul_com1).intersection(set(mul_com2)))
+    return mul_com[:5]
+print("K. Cinci multipli comuni.", multipli_comuni(a, b))
 
 #L. Cifrele care se contin in ambele numere
 def cifre(x, y):
@@ -99,7 +99,7 @@ def cifre(x, y):
     l2 = set(map(int, str(y)))
     l = l1.intersection(l2)
     return l
-print("L. Cifrele care se contin in ambele numere", cifre(a, b))
+print("L. Cifrele care se contin in ambele numere.", cifre(a, b))
 
 #M. Cifrele care sunt in primul numar, dar nu sunt in al doilea
 def cifre_nr(x, y):
@@ -109,7 +109,7 @@ def cifre_nr(x, y):
         if i in l2:
             l1.remove(i)
     return l1
-print("M. Cifrele care sunt in primul numar, dar nu sunt in al doilea", cifre_nr(a, b))
+print("M. Cifrele care sunt in primul numar, dar nu sunt in al doilea.", cifre_nr(a, b))
 
 #N. Numere prietene
 def prietene(x, y):
@@ -124,4 +124,4 @@ def prietene(x, y):
     if len(divizori1)==len(divizori2):
         n = "PRIETENE"
     return n
-print("N. Numere prietene", prietene(a, b))
+print("N. Sunt numerele prietene sau nu.", prietene(a, b))
